a386a6094b683d11b7960ca9f7da0dc488eeb4b6
Osama Sheikh <44756471+Osshaikh@users.noreply.github.com>
2023-06-27 11:45:03 +0530

    update postgresql;

--- azureproject/production.py
+++ azureproject/production.py
@@ -3,8 +3,8 @@ import os
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.getenv('SECRET_KEY')
 
-ALLOWED_HOSTS = [os.environ['WEBSITE_HOSTNAME']] if 'WEBSITE_HOSTNAME' in os.environ else []
-CSRF_TRUSTED_ORIGINS = ['https://' + os.environ['WEBSITE_HOSTNAME']] if 'WEBSITE_HOSTNAME' in os.environ else []
+ALLOWED_HOSTS = ['postgresqlapp.azurewebsites.net'] 
+CSRF_TRUSTED_ORIGINS = ['https://postgresqlapp.azurewebsites.net'] 
 
 # Configure Postgres database based on connection string of the libpq Keyword/Value form
 # https://www.postgresql.org/docs/current/libpq-connect.html#LIBPQ-CONNSTRING
@@ -12,8 +12,8 @@ conn_str = os.environ['AZURE_POSTGRESQL_CONNECTIONSTRING']
 conn_str_params = {pair.split('=')[0]: pair.split('=')[1] for pair in conn_str.split(' ')}
 
 DATABASE_URI = 'postgresql+psycopg2://{dbuser}:{dbpass}@{dbhost}/{dbname}'.format(
-    dbuser=conn_str_params['user'],
-    dbpass=conn_str_params['password'],
-    dbhost=conn_str_params['host'], + ".postgres.database.azure.com",
-    dbname=conn_str_params['dbname']
+    dbuser=conn_str_params['kzswjjcdtf'],
+    dbpass=conn_str_params['KE7TJ1C2H4VF0A75$'],
+    dbhost=conn_str_params['postgresqlapp-server'] +".postgres.database.azure.com",
+    dbname=conn_str_params['postgresqlapp-database']
 )
